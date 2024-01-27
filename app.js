48ec0d2d6553694d87cd4a6379582cd9b7b6d394
Saran <saranmvdm@gmail.com>
2024-01-27 22:56:27 +0530

    fix bug pdf saved in server

--- app.js
+++ app.js
@@ -16,13 +16,31 @@ app.post("/generatepdf", async (req, res) => {
     console.log("new page opend");
     await page.goto(url, { waitUntil: "networkidle2"});
     console.log("page loaded");
-    const pdfgenerate = await page.pdf({ format: "A4" });
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          let totalHeight = 0;
+          const distance = 100; // Distance to scroll in each step
+          const maxScrolls = 1000; // Maximum number of scrolls to prevent infinite scrolling
+    
+          const timer = setInterval(() => {
+            const scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+    
+            // Stop scrolling if reached the bottom of the page or exceeded maximum scrolls
+            if (totalHeight >= scrollHeight || totalHeight >= maxScrolls * distance) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100); // Interval between scrolls in milliseconds
+        });
+      });
+    const pdfgenerate = await page.pdf({ path:"output.pdf",printBackground: true, format: "A4" });
     console.log("pdf generated");
     await browser.close();
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "attachment; filename=output.pdf");
-    res.send(pdfgenerate);
-    console.log("pdf sended");
+    // res.setHeader("Content-Type", "application/pdf");
+    // res.setHeader("Content-Disposition", "attachment; filename=output.pdf");
+    // res.send(pdfgenerate);
   } catch (error) {
     console.log(error);
     res.status(500).send('Error generating PDF');
